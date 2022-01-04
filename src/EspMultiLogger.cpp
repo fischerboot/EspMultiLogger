6aa32416489e6ead164aa2fbb7a094e172bcc149
Marcel Fischer <Fischerboot-sinkt@web.de>
2022-01-04 19:47:32 +0100

    first implementation untested

---
+++ src/EspMultiLogger.cpp
@@ -0,0 +1,24 @@
+// inspired from https://github.com/atomic14/ESP32RemoteLogging
+
+#include "EspMultiLogger.h"
+
+EspMultiLogger::EspMultiLogger(LogLevel level) {
+  mLevel = level;
+  mBufferPos = 0;
+}
+
+size_t EspMultiLogger::write(uint8_t c) {
+  // check to see if we've reached the end of our buffer or received a newline character
+  if (c == '\n' || mBufferPos == BUFFER_SIZE - 1) {
+    // add a null terminating byte to the buffer
+    mBuffer[mBufferPos] = 0;
+    // send the message to the Serial port
+    Serial.println((const char *) mBuffer);
+    mBufferPos = 0;
+  } else {
+    // buffer the character up for sending later
+    mBuffer[mBufferPos] = c;
+    mBufferPos++;
+  }
+  return 1;
+}
---
+++ src/EspMultiLogger.h
@@ -0,0 +1,29 @@
+#ifndef __esp_multi_logger_h__
+#define __esp_multi_logger_h__
+
+// inspired from https://github.com/atomic14/ESP32RemoteLogging
+
+enum LogLevel {
+  Error = 3,
+  Warning = 4,
+  Notice = 5,
+  Info = 6,
+  Debug = 7
+};
+
+#define BUFFER_SIZE 200
+
+class EspMultiLogger: public Print  {
+  private:
+    LogLevel mLevel;
+
+    uint8_t mBuffer[BUFFER_SIZE];
+    int mBufferPos;
+    
+  public:
+    EspMultiLogger(LogLevel level);
+    size_t write(uint8_t c);
+    setLogLevel(LogLevel);
+};
+
+#endif
