b5dd81a411e8f17f5384a640969a08e8f7ce9fc9
Marcel Fischer <fischerboot-sinkt@web.de>
2025-09-20 09:49:26 +0200

    update description

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# EspMultiLogger
+### EspMultiLogger
 This Library should facilitate an logger which generates from a single input multiple outputs on ESPs (e,g, Serialoutput, Telnet, MQTT and many more)
+## Usage
+
+## Startup
+For each Loglevel a new EspMultilogger instance is needed. 
+```
+  InfoLogger = new EspMultiLogger(Info);
+  DebugLogger = new EspMultiLogger(Debug);
+```
+The whole (only once during startup for all logger) MultiLogger needed to be initalized during startup. (This could be necessary for different logger types. e.g. Serial must be intialized or simliar.)
+The Same applies for the SetLogLevel as well. (planned to be opitmized out with a default value)
+```
+  EspMultiLogger::setLogLevel(Debug);
+  EspMultiLogger::initLogger();
+```
+## loop
+
+The EspMultilogger need to be called regularly to send out its logs or supervise the connections. 
+
+```
+EspMultiLogger::loopLogger();
+```
+
+## Logging
+```
+InfoLogger->println("LED is on"); 
+```
---
+++ examples/main.cpp
@@ -0,0 +1,56 @@
+/*********
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/vs-code-platformio-ide-esp32-esp8266-arduino/
+*********/
+
+#include <Arduino.h>
+#include <EspMultiLogger.h>
+#include "WlanConfig.h"
+/*
+Configuration
+*/
+#define LED 2
+
+#ifndef WlanConfig_h
+#define STASSID "------------"
+#define STAPSK  "------------"
+#endif
+const char* ssid = STASSID;
+const char* password = STAPSK;
+
+EspMultiLogger* InfoLogger;
+EspMultiLogger* DebugLogger;
+bool on = true;
+void setup() {
+  pinMode(LED, OUTPUT);
+  // W-Lan Activating
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+  
+  InfoLogger = new EspMultiLogger(Info);
+  DebugLogger = new EspMultiLogger(Debug);
+  EspMultiLogger::setLogLevel(Debug);
+  EspMultiLogger::initLogger();
+}
+
+void loop() {
+  // Logger need to be looped
+  EspMultiLogger::loopLogger();
+  // put your main code here, to run repeatedly:
+  if(millis()%1000==0){
+    if(on == true){
+      on = false;
+      digitalWrite(LED, HIGH);
+      InfoLogger->println("LED is on");
+    }else{
+      on = true;
+      digitalWrite(LED, LOW);
+      InfoLogger->println("LED is off");
+    }
+  }
+}
